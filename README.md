55ca2d3e71c5aace5f6dfe2beb55df670c21d353
--replace-all <--replace-all>
2022-10-18 13:10:34 +0300

    complete setup

---
+++ README.md
@@ -0,0 +1,75 @@
+# RUBY FUNDAMENTALS
+This repository will be used as the guide during the lecture on fundamentals of the Ruby language.
+
+This repo contains a list of `TODOs`. Ensure you complete all of them.
+
+## Tools Used
+This project was built with the following tools:
+
+- [Ruby ~ v3.1.+](https://www.ruby-lang.org/en/)
+
+## Topics Covered
+The following are the concepts covered in this practice session
+
+- Common Data Types and Variables in Ruby
+- Methods in Ruby
+
+Documentation on the Ruby language can be found here: [Ruby Docs](https://docs.ruby-lang.org/en/3.1/)
+
+## Pre-requisites
+In order to use this repo you need to have the following installed:
+
+- OS [either: Windows 10+, Linux or MacOS(running on x86 or arm architecture)]
+- Ruby - 3.1.+
+
+## Installation
+
+To use this repo on your machine requires some simple steps
+
+### Alternative One
+
+- Open a terminal / command line interface on your computer
+- Clone the repo by using the following:
+
+        git clone https://github.com/skylar-lorena/ruby-fundamentals.git
+
+- Be patient as it creates a copy on your local machine for you.
+- Change directory to the repo folder:
+
+        cd l3-ruby-fundamentals
+
+- (Optional) Open it in ``Visual Studio Code``
+
+        code .
+
+- (Alternate Option) Open it in any editor of your choice.
+- Hurray! You are one step closer to being as intelligent as Einstein.
+
+### Alternative Two
+
+- On the top right corner of this page there is a button labelled ``Fork``.
+- Click on that button to fork the repo to your own account.
+- Take on the process in ``Alternative One`` above.
+- Remember to replace your username when cloning.
+
+        git clone https://github.com/your-username-here/ruby-fundamentals.git
+
+
+# Running the application
+
+Running the application is very straight forward. You can use the following steps to run the app.
+
+- Install required dependencies using bundle
+
+      bundle install
+
+- Run the application on the terminal
+
+      ruby main.rb
+
+# Authors
+This project was contributed to by:
+- [Lorenah M](https://github.com/skylar-lorena/)
+
+# License
+The project is licensed under Apache 2.0.
---
+++ main.rb
@@ -0,0 +1,115 @@
+# COMMENTS ARE IN HASHES
+#Ruby is an object-oriented, reflective, general-purpose, dynamic programming language. 
+
+
+# 1.0 Printing Methods
+# TODO: puts
+#It will print the string along with a line break at the end.
+puts "Hello World"
+puts "Hello World"
+
+# TODO: print
+#It will print the string without a line break at the end.
+print "Hello World"
+print "Hello World"
+
+
+# TODO: pp
+#It will print the string in a pretty format.
+nested_students = [
+    ["Mike", "Grade 10", "A average"],
+    ["Tim", "Grade 10", "C average"],
+    ["Monique", "Grade 11", "B average"]
+  ]
+pp nested_students
+
+# 2.0 Strings
+# TODO: CREATE A STRING
+#We define strings with either single quotes or double quotes
+#You can also create a new string by using the String class constructor method
+String.new("Just a string")
+
+# TODO: CREATE A STRING WITH SINGLE QUOTES
+puts 'Hello World'
+
+# TODO: INTERPOLATE STRINGS
+#To use string interpolation in Ruby, use double quotes
+my_name = "John"
+puts "Hello #{my_name}"
+
+# TODO: String methods (upcase, downcase, capitalize)
+#You can use the upcase, downcase, and capitalize methods to change the case of a string
+puts "Hello World".upcase
+puts "Hello World".downcase
+puts "Hello World".capitalize
+
+# 3.0 Numbers
+# TODO: Integers
+#Integers are whole numbers
+#You can use the Integer class constructor
+Integer.new(5)
+
+
+# TODO: Float
+#Floats are numbers with decimals
+#You can use the Float class constructor
+Float.new(5.5)
+
+
+# TODO: Number methods (floor, ceil, to_i, to_f)
+#You can use the floor, ceil, to_i, and to_f methods to change the type of a number
+puts 5.5.floor # => 5 returns the largest integer less than or equal to num
+puts 5.5.ceil # => 6 returns the smallest integer greater than or equal to num
+puts 5.5.to_i # => 5 returns the integer part of a float
+puts 5.to_f # => 5.0 returns the float representation of a number
+
+# 4.0 Boolean
+# TODO: True / Truthy
+#True is a boolean value that represents truth
+#Truthy is a boolean value that represents truth
+#You can use the TrueClass class constructor
+TrueClass.new(true) # => true
+
+# TODO: False / Falsy
+#False is a boolean value that represents falsity
+#Falsy is a boolean value that represents falsity
+#You can use the FalseClass class constructor
+FalseClass.new(false) # => false
+
+
+# 5.0 Symbols
+# TODO: Create symbol
+#Symbols are immutable strings
+#You can use the Symbol class constructor
+Symbol.new(:symbol)
+
+
+# 6.0 Arrays
+# TODO: Create array natively
+#Charlie Group
+
+
+# TODO: Create array using Array class
+#Charlie Group
+
+
+# 7.0 Hashes
+# TODO: Create Hash
+#Delta Group
+
+# TODO: Create Hash using Hash class
+#Delta Group
+
+# 8.0 Methods
+# TODO: Ruby Method with enclosed params
+
+
+# TODO: Ruby Method with open params
+
+
+
+
+
+
+
+
